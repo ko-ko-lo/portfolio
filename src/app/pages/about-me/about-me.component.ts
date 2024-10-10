cdcf04ed252d1ec97192c6d26bdd1ea2066f9589
ko-ko-lo <d-kolodzey@web.de>
2024-10-10 14:57:22 +0200

    Adding About me Page

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ContactComponent } from '../../components/contact/contact.component';
+import { IntroComponent } from '../../components/intro/intro.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, IntroComponent, ContactComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  title: string = 'From Craft to Code: My Creative Journey';
+  text: string =
+    'This section tells the story of how I evolved, step-by-step, through Art, Design and Software Development.';
+}
