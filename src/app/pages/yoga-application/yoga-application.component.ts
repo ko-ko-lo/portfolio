d82fdbaa1d996970d6159b44dc347e6b30bad702
ko-ko-lo <d-kolodzey@web.de>
2024-10-11 13:11:25 +0200

    Bugfixing and working on Project Page

--- src/app/pages/yoga-application/yoga-application.component.ts
+++ src/app/pages/yoga-application/yoga-application.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { IntroComponent } from '../../components/intro/intro.component';
 
 @Component({
   selector: 'app-yoga-application',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, IntroComponent],
   templateUrl: './yoga-application.component.html',
   styleUrl: './yoga-application.component.scss',
 })
-export class YogaApplicationComponent {}
+export class YogaApplicationComponent {
+  title: string = 'Bringing Yoga to Life: My Heart Project';
+  text: string[] = [
+    'This project represents the culmination of my personal journey through Yoga, Art, Design and Development - a heart project that allowed me to bring together all the things I love most. Having practiced yoga for many years and completed both Traditional and Yin Yoga Teacher Trainings, I found that yoga provided not only physical benefits but also a space for self-reflection, healing, and growth. I wanted to create something that reflected my passion for yoga while also leveraging my skills in UX/UI Design, Illustration and Software Development.',
+    'The Yin-Style Yoga Application is the result of that desire - a space where users can explore and learn about Yin-Style Yoga.',
+  ];
+}
