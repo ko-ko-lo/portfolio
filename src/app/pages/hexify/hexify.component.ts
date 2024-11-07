aebe78dd43072499e920d596c3658841c12b5173
ko-ko-lo <d-kolodzey@web.de>
2024-11-07 18:40:04 +0100

    Added structure and content to Hexify product page

--- src/app/pages/hexify/hexify.component.ts
+++ src/app/pages/hexify/hexify.component.ts
@@ -12,6 +12,15 @@ import { IntroComponent } from '../../components/intro/intro.component';
 export class HexifyComponent {
   title: string = 'Hexify Plugin: Seamlessly Bring Figma Colors to Code';
   text: string[] = [
-    'After repeatedly transferring color collections manually from Figma to SCSS, I developed this plugin to streamline the process. Hexify extracts color variables directly from Figma collections, converting them into hex values and generating SCSS, CSS, or JSON files.',
+    'Creating Hexify started from a simple need: I wanted a way to effortlessly convert Figma color collections into code, without the tedious task of transferring colors manually. So I made Hexify - a small tool that turns color collections into SCSS, CSS, or JSON, all ready for use with just a few clicks.',
+    "At first, I was just curious to see if I could build a Figma plugin, but soon I realized that this could be a meaningful addition to my workflow - and maybe even a stepping stone for creating other helpful tools. Hexify isn't just about automating a task, it's also my invitation to explore new ideas, learn new technologies, and share something that makes creative work just a little easier.",
   ];
+
+  scrollTo(element: string): void {
+    (document.getElementById(element) as HTMLElement).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
 }
