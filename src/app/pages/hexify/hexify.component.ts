744acc55a1ed623cfedee6daf722b784ebe0a6dd
ko-ko-lo <d-kolodzey@web.de>
2024-11-08 12:51:56 +0100

    Finished Hexify Product Page

--- src/app/pages/hexify/hexify.component.ts
+++ src/app/pages/hexify/hexify.component.ts
@@ -16,6 +16,27 @@ export class HexifyComponent {
     "At first, I was just curious to see if I could build a Figma plugin, but soon I realized that this could be a meaningful addition to my workflow - and maybe even a stepping stone for creating other helpful tools. Hexify isn't just about automating a task, it's also my invitation to explore new ideas, learn new technologies, and share something that makes creative work just a little easier.",
   ];
 
+  codeSnippet: string = `
+  export async function getColorVariablesByIds(
+    variableIds: string[]
+  ): Promise<Variable[]> {
+    const variables = await Promise.all(
+      variableIds.map(async (id) => {
+        try {
+          return await figma.variables.getVariableByIdAsync(id);
+        } catch (error) {
+          console.error('Error retrieving variable with ID:', error);
+          return null;
+        }
+      })
+    );
+
+    return variables.filter(
+      (variable): variable is Variable =>
+        variable !== null && variable.resolvedType === "COLOR"
+    );
+  }`;
+
   scrollTo(element: string): void {
     (document.getElementById(element) as HTMLElement).scrollIntoView({
       behavior: 'smooth',
