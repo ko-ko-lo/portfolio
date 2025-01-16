f909020d948b7523fefdb337334a3225c53c28a3
ko-ko-lo <d-kolodzey@web.de>
2025-01-16 13:47:36 +0100

    Refinement Project Section

--- src/app/pages/project-content/hexify/hexify.component.ts
+++ src/app/pages/project-content/hexify/hexify.component.ts
@@ -12,8 +12,7 @@ import { IntroComponent } from '../../../components/intro/intro.component';
 export class HexifyComponent {
   title: string = 'Hexify Plugin: Seamlessly Bring Figma Colors to Code';
   text: string[] = [
-    'Creating Hexify started from a simple need: I wanted a way to effortlessly convert Figma color collections into code, without the tedious task of transferring colors manually. So I made Hexify - a small tool that turns color collections into SCSS, CSS, or JSON, all ready for use with just a few clicks.',
-    "At first, I was just curious to see if I could build a Figma plugin, but soon I realized that this could be a meaningful addition to my workflow - and maybe even a stepping stone for creating other helpful tools. Hexify isn't just about automating a task, it's also my invitation to explore new ideas, learn new technologies, and share something that makes creative work just a little easier.",
+    'Creating Hexify started from a simple need: I wanted a way to effortlessly convert Figma Color Collections into Code, without the tedious task of transferring Color Values manually. So I made Hexify - a small tool that turns Color Collections into SCSS, CSS, or JSON, all ready for use with just a few clicks.',
   ];
 
   codeSnippet: string = `
