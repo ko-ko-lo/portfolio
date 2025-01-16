fe8f60e1f61a6d17dfe1eba0d58aa7d28b6b5fef
ko-ko-lo <d-kolodzey@web.de>
2025-01-16 19:44:05 +0100

    Update Jsonify project page

--- src/app/pages/project-content/jsonify/jsonify.component.ts
+++ src/app/pages/project-content/jsonify/jsonify.component.ts
@@ -12,8 +12,7 @@ import { IntroComponent } from '../../../components/intro/intro.component';
 export class JsonifyComponent {
   title: string = 'JSONify Plugin: Export Text into Structured JSON Data';
   text: string[] = [
-    'JSONify was born out of a simple frustration. While working on a project, I found myself constantly copying text from Figma to structure it for my JSON files. It was repetitive, tedious, and disrupted my flow. So I built JSONify. A plugin that can turn text from Figma layers into structured JSON files in seconds. It respects layer names, groups, and hierarchies, so the content stays organized.',
-    "This plugin isn't just about efficiency, it's a little reflection of the joy I find in creating tools that make creative work smoother. JSONify is part of my journey, learning and building as I go.",
+    'JSONify was born out of a simple frustration: while working on a project, I found myself constantly copying text from Figma to structure it for my JSON files. It was repetitive, tedious, and disrupted my flow. So I built JSONify. A Plugin that can turn text into structured JSON files in seconds.',
   ];
 
   codeSnippet: string = `
