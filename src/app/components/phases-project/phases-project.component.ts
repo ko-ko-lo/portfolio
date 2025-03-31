0f611bae0bca08a0f9df3106e57d0c638ea4d460
ko-ko-lo <d-kolodzey@web.de>
2025-03-31 23:19:31 +0200

    Created and implemented a component for project phases

---
+++ src/app/components/phases-project/phases-project.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-phases-project',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './phases-project.component.html',
+  styleUrl: './phases-project.component.scss',
+})
+export class PhasesProjectComponent {
+  @Input() phasesCount: string = '';
+  @Input() phasesText: string = '';
+  @Input() phasesProgress: string = '';
+  @Input() phasesHeadline: string = '';
+  @Input() phasesLink: string = '';
+  @Input() phasesImgWebp: string = '';
+  @Input() phasesImg: string = '';
+  @Input() isSwapped: boolean = false;
+}
